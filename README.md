21edf2465bb4c22129a90fc38b4a3ce50dc26a9b
gmind <r3tga@yahoo.com>
2021-02-22 15:03:00 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# clubhouse_inviter
+Копируем скрипт для рассылки инвайтов
+
+$ git clone https://github.com/grvmind/clubhouse_inviter.git
+$ cd clubhouse_inviter
+
+Устанавливаем clubhouse-py
+
+$ git clone https://github.com/stypr/clubhouse-py.git clubhouse
+$ cd clubhouse
+$ pip3 install -r requirements.txt
+
+Запускаем клиент, авторизуемся, чтобы записать id в сеттинги
+
+$ python3 cli.py
+
+Переходим назад, запускаем скрипт, вводим данные
+
+$ python3 clubhouse_inviter.py
+
+Делаем цепочку :)
---
+++ clubhouse_inviter.py
@@ -0,0 +1,19 @@
+from clubhouse.clubhouse.clubhouse import Clubhouse
+import configparser
+
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read('clubhouse/setting.ini')
+    user_device = config.get('Account', 'user_device')
+    user_id = config.get('Account', 'user_id')
+    user_token = config.get('Account', 'user_token')
+
+    clubhouse = Clubhouse(
+        user_id=user_id,
+        user_token=user_token,
+        user_device=user_device
+    )
+    # print(clubhouse.me())
+    name = input('Input name: ')
+    phone_number = input('Input phone number with \"+\" as +79991234567: ')
+    print(clubhouse.invite_to_app(name=name, phone_number=phone_number))
